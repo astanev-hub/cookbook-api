4a78dd7a7ca098d77d7e74dc0e685aa0354af886
Cristiano Neves <cris.a.neves@gmail.com>
2022-03-17 22:35:38 -0300

    Ingrediente JPA Entity mapping

---
+++ src/main/java/br/com/letscode/cookbook/cookbookapi/model/Ingrediente.java
@@ -0,0 +1,25 @@
+package br.com.letscode.cookbook.cookbookapi.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "ingrediente", schema = "cookbook")
+@Getter
+@Setter
+@EqualsAndHashCode()
+public class Ingrediente {
+    @Id
+    @Column(name = "nome", length = 120)
+    @EqualsAndHashCode.Include
+    private String nome;
+
+    @Column(name = "descricao", length = 250)
+    private String descricao;
+}
