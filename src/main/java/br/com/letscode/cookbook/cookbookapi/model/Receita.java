ff9bede22d4902961cd946d8e73be48af6d58561
Cristiano Neves <cris.a.neves@gmail.com>
2022-03-22 19:48:45 -0300

    Added basic model for Receita entity

---
+++ src/main/java/br/com/letscode/cookbook/cookbookapi/model/Receita.java
@@ -0,0 +1,32 @@
+package br.com.letscode.cookbook.cookbookapi.model;
+
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Set;
+
+@Entity
+@Table(name = "receita", schema = "cookbook")
+@Getter
+@Setter
+public class Receita {
+    @Id
+    @EqualsAndHashCode.Include
+    @Column(name = "nome", length = 120)
+    private String nome;
+
+    @Column(name = "categoria", length = 30)
+    private String categoria;
+
+    @Column(name = "tempo_preparo")
+    private Integer tempoPreparo;
+
+    @Column(name = "rendimento_tipo", length = 30)
+    private String rendimentoTipo;
+
+    @Column(name = "rendimento_quantidade")
+    private Integer rendimentoQuantidade;
+
+}
