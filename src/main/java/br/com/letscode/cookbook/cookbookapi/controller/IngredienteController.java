5b7e8786f9bd3a62a764ec852533e9eb7a478597
Cristiano Neves <cris.a.neves@gmail.com>
2022-03-24 22:17:04 -0300

    Added basic CRUD operations to IngredienteController

--- src/main/java/br/com/letscode/cookbook/cookbookapi/controller/IngredienteController.java
+++ src/main/java/br/com/letscode/cookbook/cookbookapi/controller/IngredienteController.java
@@ -2,10 +2,10 @@ package br.com.letscode.cookbook.cookbookapi.controller;
 
 import br.com.letscode.cookbook.cookbookapi.model.Ingrediente;
 import br.com.letscode.cookbook.cookbookapi.repository.IngredienteRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.client.RestClientException;
 
 import java.util.List;
 
@@ -25,6 +25,41 @@ public class IngredienteController {
 
     @GetMapping(path = "/{nome}")
     public Ingrediente findByName(@PathVariable String nome) {
-        return repository.getById(nome);
+        return repository.findById(nome).orElse(null);
+    }
+
+    @GetMapping(path = "/filter")
+    public Page<Ingrediente> filter(Pageable pageable) {
+        return repository.findAll(pageable);
+    }
+
+    @PostMapping
+    public Ingrediente add(Ingrediente ingrediente) {
+        if (repository.findById(ingrediente.getNome()).isPresent()) {
+            throw new RestClientException("Ingrediente já existe!!!");
+        }
+        return repository.save(ingrediente);
+    }
+
+    @PutMapping
+    public Ingrediente update(Ingrediente ingrediente) {
+        Ingrediente entity = repository.findById(ingrediente.getNome()).orElse(null);
+        if (entity == null)
+            throw new RestClientException("Ingrediente não existente!!!");
+
+        entity.setDescricao(ingrediente.getDescricao());
+        return repository.save(entity);
+    }
+
+    @DeleteMapping("/{nome}")
+    public Ingrediente delete(@PathVariable String nome) {
+//        repository.deleteById(nome);
+
+        Ingrediente entity = repository.findById(nome).orElse(null);
+        if (entity == null)
+            throw new RestClientException("Ingrediente não existente!!!");
+
+        repository.delete(entity);
+        return entity;
     }
 }
